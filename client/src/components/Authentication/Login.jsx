ef1705b80df621d2a0a231d3c8c94a45f4de52d4
Hyper-ActiveX <jayactive11112@gmail.com>
2024-05-17 12:50:40 +0530

    authentication integration done but reflected not done in frontend side

--- client/src/components/Authentication/Login.jsx
+++ client/src/components/Authentication/Login.jsx
@@ -1,5 +1,9 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {  userState } from "../../slice/userSlice";
+import { signIn } from "../../middleware/auth";
 import { useAppContext } from "../Appcontext";
+
 
 // Material UI Imports
 import {
@@ -19,24 +23,68 @@ import {
 import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
 import LoginIcon from "@mui/icons-material/Login";
+import { useNavigate } from "react-router-dom";
 
 // Email Validation
 const isEmail = (email) =>
   /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(email);
 
 export default function Login() {
+  //Jay's code start
+
+  const userStates = useSelector(userState);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const [details, setDetails] = useState({
+    credential: "",
+    password: "",
+  });
+
+  const [errors, setErrors] = useState({
+    credential: false,
+    password: false,
+  });
+
+  function changeHandler(e) {
+    if (e.target.name === "credential") {
+      setErrors({ ...errors, credential: false });
+    }
+    if (e.target.name === "password") {
+      setErrors({ ...errors, password: false });
+    }
+
+    setDetails({ ...details, [e.target.name]: e.target.value });
+    
+  }
+
+  const submitHandler = async (e) =>{
+    e.preventDefault();
+    if(details.credential === "" && details.password === ""){
+      setErrors({credential: true, password: true});
+      return ;
+    }
+    if(details.credential === "") setErrors({...errors, credential: true});
+    if (details.password === "") setErrors({ ...errors, password: true });
+
+    // console.log(details)
+    await signIn(details, dispatch, navigate);
+  }
+
+
+  // Jay's code end
   const [showPassword, setShowPassword] = React.useState(false);
 
   //Inputs
-  const [emailInput, setEmailInput] = useState();
-  const [passwordInput, setPasswordInput] = useState();
+  // const [emailInput, setEmailInput] = useState();
+  // const [passwordInput, setPasswordInput] = useState();
   const [rememberMe, setRememberMe] = useState();
 
-  // Inputs Errors
-  const [emailError, setEmailError] = useState(false);
-  const [passwordError, setPasswordError] = useState(false);
+  // // Inputs Errors
+  // const [emailError, setEmailError] = useState(false);
+  // const [passwordError, setPasswordError] = useState(false);
 
-  // Overall Form Validity
+  // // Overall Form Validity
   const [formValid, setFormValid] = useState();
   const [success, setSuccess] = useState();
 
@@ -50,58 +98,58 @@ export default function Login() {
   const label = { inputProps: { "aria-label": "Checkbox demo" } };
 
   // Validation for onBlur Email
-  const handleEmail = () => {
-    console.log(isEmail(emailInput));
-    if (!isEmail(emailInput)) {
-      setEmailError(true);
-      return;
-    }
+  // const handleEmail = () => {
+  //   console.log(isEmail(emailInput));
+  //   if (!isEmail(emailInput)) {
+  //     setEmailError(true);
+  //     return;
+  //   }
 
-    setEmailError(false);
-  };
+  //   setEmailError(false);
+  // };
 
   // Validation for onBlur Password
-  const handlePassword = () => {
-    if (
-      !passwordInput ||
-      passwordInput.length < 5 ||
-      passwordInput.length > 20
-    ) {
-      setPasswordError(true);
-      return;
-    }
+  // const handlePassword = () => {
+  //   if (
+  //     !passwordInput ||
+  //     passwordInput.length < 5 ||
+  //     passwordInput.length > 20
+  //   ) {
+  //     setPasswordError(true);
+  //     return;
+  //   }
 
-    setPasswordError(false);
-  };
+  //   setPasswordError(false);
+  // };
 
   //handle Submittion
-  const handleSubmit = () => {
-    setSuccess(null);
-    //First of all Check for Errors
+  // const handleSubmit = () => {
+  //   setSuccess(null);
+  //   //First of all Check for Errors
 
-    // If Email error is true
-    if (emailError || !emailInput) {
-      setFormValid("Email is Invalid. Please Re-Enter");
-      return;
-    }
+  //   // If Email error is true
+  //   if (emailError || !emailInput) {
+  //     setFormValid("Email is Invalid. Please Re-Enter");
+  //     return;
+  //   }
 
-    // If Password error is true
-    if (passwordError || !passwordInput) {
-      setFormValid(
-        "Password is set btw 5 - 20 characters long. Please Re-Enter"
-      );
-      return;
-    }
-    setFormValid(null);
+  //   // If Password error is true
+  //   if (passwordError || !passwordInput) {
+  //     setFormValid(
+  //       "Password is set btw 5 - 20 characters long. Please Re-Enter"
+  //     );
+  //     return;
+  //   }
+  //   setFormValid(null);
 
-    // Proceed to use the information passed
-    console.log("Email : " + emailInput);
-    console.log("Password : " + passwordInput);
-    console.log("Remember : " + rememberMe);
+  //   // Proceed to use the information passed
+  //   console.log("Email : " + emailInput);
+  //   console.log("Password : " + passwordInput);
+  //   console.log("Remember : " + rememberMe);
 
-    //Show Successfull Submittion
-    setSuccess("Form Submitted Successfully");
-  };
+  //   //Show Successfull Submittion
+  //   setSuccess("Form Submitted Successfully");
+  // };
 
   const{ loginchk, setLogin
   }=useAppContext()
@@ -113,36 +161,48 @@ export default function Login() {
         <TextField
           label="Email Address"
           fullWidth
-          error={emailError}
+          // error={emailError}
           id="standard-basic"
           variant="standard"
           sx={{ width: "100%" }}
-          value={emailInput}
+          // value={emailInput}
           InputProps={{}}
           size="small"
-          onBlur={handleEmail}
-          onChange={(event) => {
-            setEmailInput(event.target.value);
-          }}
+          // onBlur={handleEmail}
+          // onChange={(event) => {
+          //   setEmailInput(event.target.value);
+          // }}
+          autoComplete="email"
+          name="credential"
+          onChange={changeHandler}
+          value={details.credential}
+          error={errors.credential}
+
         />
       </div>
       <div style={{ marginTop: "5px" }}>
         <FormControl sx={{ width: "100%" }} variant="standard">
           <InputLabel
-            error={passwordError}
+            // error={passwordError}
             htmlFor="standard-adornment-password"
           >
             Password
           </InputLabel>
           <Input
-            error={passwordError}
-            onBlur={handlePassword}
+            // error={passwordError}
+            // onBlur={handlePassword}
             id="standard-adornment-password"
             type={showPassword ? "text" : "password"}
-            onChange={(event) => {
-              setPasswordInput(event.target.value);
-            }}
-            value={passwordInput}
+            // onChange={(event) => {
+            //   setPasswordInput(event.target.value);
+            // }}
+            // value={passwordInput}
+
+            name="password"
+            value={details.password}
+            onChange={changeHandler}
+            error={errors.password}
+
             endAdornment={
               <InputAdornment position="end">
                 <IconButton
@@ -174,7 +234,8 @@ export default function Login() {
           variant="contained"
           fullWidth
           startIcon={<LoginIcon />}
-          onClick={handleSubmit}
+          // onClick={handleSubmit}
+          onClick={submitHandler}
         >
           LOGIN
         </Button>
