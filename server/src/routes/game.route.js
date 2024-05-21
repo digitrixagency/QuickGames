4b0bc25f1c71b3088d6eab2234598c54f66183a3
krish <kaps61929>
2024-05-21 12:59:35 +0530

    .

--- server/src/routes/game.route.js
+++ server/src/routes/game.route.js
@@ -5,13 +5,8 @@ const VerifyToken = require("../middlewares/auth.middleware");
 const gameRoutes = Router();
 
 
-<<<<<<< Updated upstream
 gameRoutes.get('/games/top-categories', gameController.getTopCategories); //data to display on dashboard
 gameRoutes.get('/games/category/:category',gameController.getGamesByCategory);
-=======
-
-gameRoutes.get('/games/category/:category', gameController.getGamesByCategory);
->>>>>>> Stashed changes
 gameRoutes.get('/games/subcategory/:subcategory', gameController.getGamesBySubcategory);
 gameRoutes.get('/games/search', gameController.searchGames);
 gameRoutes.get('/game/gamedetail/i/:gameId', gameController.getNameByGameId);
