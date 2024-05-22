46dbd8b45a6ca64069ae3acf158220d34832b3ef
Madan <madangopal16072000@gmail.com>
2024-05-22 08:23:21 +0530

    updated gamecontroller

--- server/src/controllers/game.controller.js
+++ server/src/controllers/game.controller.js
@@ -1,239 +1,238 @@
-// const { PrismaClient } = require('@prisma/client');
-// const prisma = new PrismaClient();
-// const {handleErrorResponse}=require('../errorResponses');
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
+const { handleErrorResponse } = require("../errorResponses");
 
-// //Finding to 10 categories to display data in Home page
-// async function getTopCategories(req, res) {
-//   try {
-//     // Aggregate data to find top 10 categories based on number of games
-//     const categories = await prisma.game.groupBy({
-//       by: ['category'],
-//       _count: {
-//         category: true,
-//       },
-//       orderBy: {
-//         _count: {
-//           category: 'desc',
-//         },
-//       },
-//       take: 10,
-//     });
+//Finding to 10 categories to display data in Home page
+async function getTopCategories(req, res) {
+  try {
+    // Aggregate data to find top 10 categories based on number of games
+    const categories = await prisma.game.groupBy({
+      by: ["category"],
+      _count: {
+        category: true,
+      },
+      orderBy: {
+        _count: {
+          category: "desc",
+        },
+      },
+      take: 10,
+    });
 
-//     // Fetch data for these top categories
-//     const categoryDataPromises = categories.map(async (category) => {
-//       const games = await prisma.game.findMany({
-//         where: {
-//           category: category.category,
-//         },
-//         orderBy: {
-//           played: 'desc',
-//         },
-//         take: 20, // Fetch top 5 games for each category, adjust as needed
-//       });
-//       return {
-//         category: category.category,
-//         gameCount: category._count.category,
-//         games,
-//       };
-//     });
+    // Fetch data for these top categories
+    const categoryDataPromises = categories.map(async (category) => {
+      const games = await prisma.game.findMany({
+        where: {
+          category: category.category,
+        },
+        orderBy: {
+          played: "desc",
+        },
+        take: 20, // Fetch top 5 games for each category, adjust as needed
+      });
+      return {
+        category: category.category,
+        gameCount: category._count.category,
+        games,
+      };
+    });
 
-//     const categoryData = await Promise.all(categoryDataPromises);
+    const categoryData = await Promise.all(categoryDataPromises);
 
-//     res.json(categoryData);
-//   } catch (error) {
-//     console.error('Error fetching top categories:', error);
-//     handleErrorResponse(res, 500);
-//   }
-// }
+    res.json(categoryData);
+  } catch (error) {
+    console.error("Error fetching top categories:", error);
+    handleErrorResponse(res, 500);
+  }
+}
 
-// async function getGamesByCategory(req, res) {
-//   const { category } = req.params;
-//   const { filter = 'new', limit = 10, page = 1 } = req.query;
-//   const offset = (page - 1) * limit;
-//   let orderBy = {};
+async function getGamesByCategory(req, res) {
+  const { category } = req.params;
+  const { filter = "new", limit = 10, page = 1 } = req.query;
+  const offset = (page - 1) * limit;
+  let orderBy = {};
 
-//   if (filter === 'new') {
-//     orderBy = { launch_year: 'desc' };
-//   } else if (filter === 'mostPlayed') {
-//     orderBy = { played: 'desc' };
-//   } else if (filter === 'mostLiked') {
-//     orderBy = { totalLikes: 'desc' };
-//   }
+  if (filter === "new") {
+    orderBy = { launch_year: "desc" };
+  } else if (filter === "mostPlayed") {
+    orderBy = { played: "desc" };
+  } else if (filter === "mostLiked") {
+    orderBy = { totalLikes: "desc" };
+  }
 
-//   try {
-//     const games = await prisma.game.findMany({
-//       where: {
-//         category: category // Assuming category is stored in lowercase
-//       },
-//       orderBy,
-//       take: Number(limit),
-//       skip: offset
-//     });
+  try {
+    const games = await prisma.game.findMany({
+      where: {
+        category: category, // Assuming category is stored in lowercase
+      },
+      orderBy,
+      take: Number(limit),
+      skip: offset,
+    });
 
-//     res.json(games);
-//   } catch (error) {
-//     console.error('Error fetching games:', error);
-//     handleErrorResponse(res,500);
+    res.json(games);
+  } catch (error) {
+    console.error("Error fetching games:", error);
+    handleErrorResponse(res, 500);
+  }
+}
 
-//   }
-// }
+async function getGamesBySubcategory(req, res) {
+  const { subcategory } = req.params;
+  const { filter = "new", limit = 10, page = 1 } = req.query;
+  const offset = (page - 1) * limit;
+  let orderBy = {};
 
-// async function getGamesBySubcategory(req, res) {
-//   const { subcategory } = req.params;
-//   const { filter = 'new', limit = 10, page = 1 } = req.query;
-//   const offset = (page - 1) * limit;
-//   let orderBy = {};
+  if (filter === "new") {
+    orderBy = { launch_year: "desc" };
+  } else if (filter === "mostPlayed") {
+    orderBy = { played: "desc" };
+  } else if (filter === "mostLiked") {
+    orderBy = { totalLikes: "desc" };
+  }
 
-//   if (filter === 'new') {
-//     orderBy = { launch_year: 'desc' };
-//   } else if (filter === 'mostPlayed') {
-//     orderBy = { played: 'desc' };
-//   } else if (filter === 'mostLiked') {
-//     orderBy = { totalLikes: 'desc' };
-//   }
+  try {
+    const games = await prisma.game.findMany({
+      where: {
+        subcategory: subcategory,
+      },
+      orderBy,
+      take: Number(limit),
+      skip: offset,
+    });
 
-//   try {
-//     const games = await prisma.game.findMany({
-//       where: {
-//         subcategory: subcategory
-//       },
-//       orderBy,
-//       take: Number(limit),
-//       skip: offset
-//     });
+    res.json(games);
+  } catch (error) {
+    console.error("Error fetching games:", error);
+    handleErrorResponse(res, 500);
+  }
+}
 
-//     res.json(games);
-//   } catch (error) {
-//     console.error('Error fetching games:', error);
-//     handleErrorResponse(res,500);
-//   }
-// }
+async function searchGames(req, res) {
+  const { searchtitle } = req.query;
+  const { limit = 10, page = 1 } = req.query;
+  const offset = (page - 1) * limit;
 
-// async function searchGames(req, res) {
-//   const { searchtitle } = req.query;
-//   const { limit = 10, page = 1 } = req.query;
-//   const offset = (page - 1) * limit;
+  try {
+    const games = await prisma.game.findMany({
+      where: {
+        title: {
+          contains: searchtitle, // Case-insensitive search by title
+        },
+      },
+      orderBy: {
+        played: "desc", // Order by most played
+      },
+      take: Number(limit),
+      skip: offset,
+    });
 
-//   try {
+    res.json(games);
+  } catch (error) {
+    console.error("Error searching games:", error);
+    handleErrorResponse(res, 500);
+  }
+}
 
-//     const games = await prisma.game.findMany({
-//       where: {
-//         title: {
-//           contains: searchtitle // Case-insensitive search by title
-//         }
-//       },
-//       orderBy: {
-//         played: 'desc' // Order by most played
-//       },
-//       take: Number(limit),
-//       skip: offset
-//     });
+async function getGameByName(req, res) {
+  const { title } = req.params;
 
-//     res.json(games);
-//   } catch (error) {
-//     console.error('Error searching games:', error);
-//     handleErrorResponse(res,500);
-//   }
-// }
+  try {
+    const game = await prisma.game.findFirst({
+      where: {
+        title: {
+          equals: title, // Case-insensitive exact match
+        },
+      },
+    });
 
-// async function getGameByName(req, res) {
-//   const { title } = req.params;
+    if (!game) {
+      return handleErrorResponse(res, 404, "Game not found");
+    }
 
-//   try {
-//     const game = await prisma.game.findFirst({
-//       where: {
-//         title: {
-//           equals: title // Case-insensitive exact match
-//         }
-//       }
-//     });
+    res.json(game);
+  } catch (error) {
+    console.error("Error getting game by name:", error);
 
-//     if (!game) {
-//       return handleErrorResponse(res,404,'Game not found' );
+    handleErrorResponse(res, 500);
+  }
+}
 
-//     }
+async function getNameByGameId(req, res) {
+  const { gameId } = req.params;
+  try {
+    const game = await prisma.game.findUnique({
+      where: {
+        id: parseInt(gameId),
+      },
+    });
 
-//     res.json(game);
-//   } catch (error) {
-//     console.error('Error getting game by name:', error);
+    if (!game) {
+      return handleErrorResponse(res, 404, "Game not found");
+    }
 
-//     handleErrorResponse(res,500);
-//   }
-// }
+    res.json({ game });
+  } catch (error) {
+    console.error("Error getting game name by ID:", error);
+    handleErrorResponse(res, 500);
+  }
+}
 
-// async function getNameByGameId(req, res) {
-//   const { gameId } = req.params;
-//   try {
-//     const game = await prisma.game.findUnique({
-//       where: {
-//         id: parseInt(gameId)
-//       }
-//     });
+async function countLikesById(req, res) {
+  const { gameId } = req.params;
 
-//     if (!game) {
-//       return handleErrorResponse(res,404,'Game not found' );
-//     }
+  try {
+    const game = await prisma.game.findUnique({
+      where: {
+        id: parseInt(gameId),
+      },
+    });
 
-//     res.json({  game});
-//   } catch (error) {
-//     console.error('Error getting game name by ID:', error);
-//     handleErrorResponse(res,500);
-//   }
-// }
+    if (!game) {
+      return handleErrorResponse(res, 404, "Game not found");
+    }
 
-// async function countLikesById(req, res) {
-//   const { gameId } = req.params;
+    res.json({ title: game.title, totalLikes: game.totalLikes });
+  } catch (error) {
+    console.error("Error counting likes by ID:", error);
+    handleErrorResponse(res, 500);
+  }
+}
 
-//   try {
-//     const game = await prisma.game.findUnique({
-//       where: {
-//         id: parseInt(gameId)
-//       }
-//     });
+async function countDislikesById(req, res) {
+  const { gameId } = req.params;
 
-//     if (!game) {
-//       return handleErrorResponse(res, 404, 'Game not found');
-//     }
+  try {
+    const game = await prisma.game.findUnique({
+      where: {
+        id: parseInt(gameId),
+      },
+      include: {
+        likes: true, // Include the likes associated with the game
+      },
+    });
 
-//     res.json({ title: game.title, totalLikes: game.totalLikes });
-//   } catch (error) {
-//     console.error('Error counting likes by ID:', error);
-//     handleErrorResponse(res, 500);
-//   }
-// }
+    if (!game) {
+      return handleErrorResponse(res, 404, "Game not found");
+    }
 
-// async function countDislikesById(req, res) {
-//   const { gameId } = req.params;
+    const dislikeCount = game.likes.filter(
+      (like) => like.like_status === -1
+    ).length; // Count dislikes
+    res.json({ title: game.title, dislikeCount });
+  } catch (error) {
+    console.error("Error counting dislikes by ID:", error);
+    handleErrorResponse(res, 500);
+  }
+}
 
-//   try {
-//     const game = await prisma.game.findUnique({
-//       where: {
-//         id: parseInt(gameId)
-//       },
-//       include: {
-//         likes: true // Include the likes associated with the game
-//       }
-//     });
-
-//     if (!game) {
-//       return handleErrorResponse(res, 404, 'Game not found');
-//     }
-
-//     const dislikeCount = game.likes.filter(like => like.like_status === -1).length; // Count dislikes
-//     res.json({ title: game.title, dislikeCount });
-//   } catch (error) {
-//     console.error('Error counting dislikes by ID:', error);
-//     handleErrorResponse(res, 500);
-//   }
-// }
-
-// module.exports = {
-//   getTopCategories,
-//   getGamesByCategory,
-//   getGamesBySubcategory,
-//   searchGames,
-//   getGameByName,
-//   getNameByGameId,
-//   countLikesById,
-//   countDislikesById
-// };
+module.exports = {
+  getTopCategories,
+  getGamesByCategory,
+  getGamesBySubcategory,
+  searchGames,
+  getGameByName,
+  getNameByGameId,
+  countLikesById,
+  countDislikesById,
+};
